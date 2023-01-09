04fdc2e1244525ef26de206560129ea800a59989
Clay <muhammadsobirturdaliyev9100@gmail.com>
2023-01-09 17:59:14 +0500

    yangi cod

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-for i in range(1, 10):
-    print(i)
+for i in range(1,6):
+    for j in range(1, 6):
+        if i == 1 or i == 5 or j == 5 or j == 1:
+            print(i, end='  ')
+        else:
+            print(end='   ')
+    print()
+
 
